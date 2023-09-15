3421e8b8bfac0b902df911d598176f55a41fbaf1
Gustafa Bayu <faafa13@gmail.com>
2023-09-15 20:42:24 +0700

    first commit

---
+++ src/test/java/com/api/movierest/MovierestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.movierest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovierestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
